fe920ea580d52ddb744923833b7e25c8e8b12220
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-22 17:02:04 +0100

    Add behaviour for validation form

---
+++ DataValidator.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace DataValidation
+{
+    partial class dataValidationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(99, 33);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(442, 22);
+            this.nameBox.TabIndex = 0;
+            // 
+            // phoneBox
+            // 
+            this.phoneBox.Location = new System.Drawing.Point(99, 86);
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(442, 22);
+            this.phoneBox.TabIndex = 1;
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(99, 141);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(442, 22);
+            this.emailBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "E-mail";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(99, 211);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(432, 211);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(109, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dataValidationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 279);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.phoneBox);
+            this.Controls.Add(this.nameBox);
+            this.Name = "dataValidationForm";
+            this.Text = "Data Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ DataValidator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DataValidation
+{
+    public partial class dataValidationForm : Form
+    {
+        Dictionary<string, string> elements = new Dictionary<string, string>();
+
+        public dataValidationForm()
+        {
+            InitializeComponent();
+            elements.Add("nameBox", "name");
+            elements.Add("phoneBox", "phone number");
+            elements.Add("emailBox", "e-mail address");
+            nameBox.TextChanged += new EventHandler(TextBox_TextChanged);
+            phoneBox.TextChanged += new EventHandler(TextBox_TextChanged);
+            emailBox.TextChanged += new EventHandler(TextBox_TextChanged);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string errorMessage = "";
+            List<TextBox> invalidInputs = GetInvalidInputs(new List<TextBox> { nameBox, phoneBox, emailBox });
+            foreach (TextBox item in invalidInputs)
+            {
+                errorMessage += (errorMessage.Length > 0) ? "\n" : "";
+                errorMessage += $"Please enter a valid {elements[item.Name]}.";
+            }
+            if (errorMessage.Length > 0)
+            {
+                foreach (TextBox item in invalidInputs)
+                {
+                    item.BackColor = Color.LightCoral;
+                }
+                DialogResult dialogResult = MessageBox.Show(errorMessage);
+                if (dialogResult == DialogResult.OK)
+                {
+                    invalidInputs[0].Focus();
+                }
+            }
+            else
+                MessageBox.Show("Data saved successfully.");
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            (sender as TextBox).BackColor = Color.White;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private List<TextBox> GetInvalidInputs(List<TextBox> inputs)
+        {
+            InputValidator validator = new InputValidator();
+            List<TextBox> invalidInputs = new List<TextBox>();
+            if (!validator.ValidName(inputs[0].Text))
+            {
+                invalidInputs.Add(inputs[0]);
+            }
+            if (!validator.ValidPhone(inputs[1].Text))
+            {
+                invalidInputs.Add(inputs[1]);
+            }
+            if (!validator.ValidEmail(inputs[2].Text))
+            {
+                invalidInputs.Add(inputs[2]);
+            }
+            return invalidInputs;
+        }
+    }
+}
