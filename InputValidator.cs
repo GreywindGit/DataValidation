acb8dfbc2e135d80ed930838e83cd7cde5f51625
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-22 16:59:43 +0100

    Add class to validate data with regexes

---
+++ InputValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DataValidation
+{
+    class InputValidator
+    {
+        public bool ValidName(string name)
+        {
+            string namePattern = @"^([A-ZÀ-Ű][A-Za-zÀ-ÿ]+\s?)+$";
+            return Regex.IsMatch(name, namePattern);
+        }
+
+        public bool ValidPhone(string phoneNumber)
+        {
+            string hungarianPhonePattern = @"^\+36[-/]((1|[237]0)[-/]\d{3}[-/]?\d{4})|([1-9]{2}[-/]\d{3}[-/]?\d{3})$";
+            return Regex.IsMatch(phoneNumber, hungarianPhonePattern);
+        }
+
+        public bool ValidEmail(string email)
+        {
+            string emailPattern = @"^[A-Za-z0-9_\.]+\@\w+\.\w{2,3}$";
+            return Regex.IsMatch(email, emailPattern);
+        }
+    }
+}
