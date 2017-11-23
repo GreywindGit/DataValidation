9ca0607b9fc18220615d589208ca90086d6ff267
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-23 11:08:41 +0100

    Add encoding converter

---
+++ EncodingConverter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace EncodingConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(@"C:\Users\Jazmin\source\Codecool\.NET basics\SI projects\DataValidation\EncodingConverter\fruits.txt"))
+                using (StreamWriter writer = new StreamWriter("happiest-utf7.txt", false, Encoding.UTF7))
+                    writer.WriteLine(reader.ReadToEnd());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
